82c915a06a7782956304aa3e564ed7c5d7ab363c
lianvervoort <lian-vervoort@hotmail.com>
2019-10-31 16:19:03 +0100

    initial commit

---
+++ .storybook/config.js
@@ -0,0 +1,15 @@
+import { configure, addParameters } from '@storybook/react';
+import { themes } from '@storybook/theming';
+// automatically import all files ending in *.stories.js
+
+import yourTheme from './yourTheme';
+
+addParameters({
+  options: {
+    theme: yourTheme,
+  },
+});
+
+
+
+configure(require.context('../src/stories', true, /\.stories\.js$/), module);
---
+++ .storybook/yourTheme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+import logo from '../joopfolio.png';
+
+export default create({
+  base: 'light',
+
+
+  brandTitle: 'Portfolio',
+  brandImage: logo,
+});
